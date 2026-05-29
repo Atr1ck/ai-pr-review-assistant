9bd56bc57dda6b09937285ecb6b51597b8bfb2a0
Atr1ck <3126670240@qq.com>
2026-05-29 11:03:10 +0800

    🔧 build: add tailwindcss

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { timerify } from 'node:perf_hooks';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), 
+            tailwindcss()
+  ],
 })
